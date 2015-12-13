2aba8b9aa49733a387d212932f98f32bd1e999c0
leo <leo@users.noreply.github.com>
2015-12-13 20:27:31 +0100

    Fix indentation

--- header.php
+++ header.php
@@ -2,14 +2,13 @@
 <html <?php language_attributes(); ?>>
 <head>
 
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <meta name="description" content="<?php bloginfo( 'description' ); ?>">
 
-	<?php wp_head(); ?>
+  <?php wp_head(); ?>
 
 </head>
 <body <?php body_class(); ?>>
-	
